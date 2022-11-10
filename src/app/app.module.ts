cba43468806e72a2acff886701c551fdd4447d67
anilegar <anilegar25@gmail.com>
2022-11-09 21:15:06 -0300

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componets/experiencia/experiencia.component';
+import { EducacionComponent } from './componets/educacion/educacion.component';
+import { HabilidadesComponent } from './componets/habilidades/habilidades.component';
+import { ProyectosComponent } from './componets/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
